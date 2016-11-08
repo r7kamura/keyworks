1e58600dafb6c095bb7b17281c82a7ef45bb5b92
r7kamura <r7kamura@gmail.com>
2016-11-08 13:20:31 +0900

    Fix lint error

--- src/javascripts/options.jsx
+++ src/javascripts/options.jsx
@@ -1,8 +1,7 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import Settings from "./components/settings.jsx";
 
 ReactDOM.render(
   <Settings/>,
-  document.getElementById("container"),
+  document.getElementById("container")
 );
