4796e94ba712f55351018e4386581fe5d53c7cc0
r7kamura <r7kamura@gmail.com>
2016-11-08 10:27:52 +0900

    Add shortcut to copy page url and title by naive impl

--- src/javascripts/background.js
+++ src/javascripts/background.js
@@ -1,1 +1,13 @@
-console.log(1);
+const copyToClipboard = (string) => {
+  const textarea = document.createElement("textarea");
+  textarea.style.cssText = "position: absolute; left: -100%";
+  document.body.appendChild(textarea);
+  textarea.value = string;
+  textarea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textarea);
+};
+
+chrome.runtime.onMessage.addListener(({ string }) => {
+  copyToClipboard(string);
+});
--- src/javascripts/content.js
+++ src/javascripts/content.js
@@ -1,7 +1,30 @@
 import { detectKeyString } from "key-string";
 
+const getPageTitle = () => {
+  return document.title;
+};
+
+const getPageUrl = () => {
+  return location.href;
+};
+
+const sendMessage = (message) => {
+  chrome.runtime.sendMessage(
+    chrome.runtime.id,
+    message,
+    {}
+  );
+};
+
 window.addEventListener("keydown", (event) => {
   const keyString = detectKeyString(event);
   if (keyString === "Ctrl+M") {
+    sendMessage({
+      string: `[${getPageTitle()}](${getPageUrl()})`,
+    });
+  } else if (keyString === "Ctrl+L") {
+    sendMessage({
+      string: `${getPageTitle()} ${getPageUrl()}`,
+    });
   }
 });
