0d9c299da407e2cc099fc012da4a07e78afb68fd
r7kamura <r7kamura@gmail.com>
2016-11-08 14:38:15 +0900

    Add some styles

--- src/javascripts/components/Settings.jsx
+++ src/javascripts/components/Settings.jsx
@@ -14,25 +14,46 @@ export default class Settings extends React.Component {
 
   render() {
     return(
-      <div className="container">
-        {
-          Object.keys(this.state.settings.actionDefinitions).map((keyString) => {
-            const definition = this.state.settings.actionDefinitions[keyString];
-            return(
-              <div key={keyString}>
-                <div>
-                  {keyString}
-                </div>
-                <div>
-                  {definition.type}
-                </div>
-                <div>
-                  {definition.template}
+      <div>
+        <header className="header">
+          <div className="section">
+            <div className="container">
+              <div className="card-transparent">
+                <h1 className="header-heading">
+                  Keyworks settings
+                </h1>
+              </div>
+            </div>
+          </div>
+        </header>
+        <main className="main">
+          <div className="section">
+            <div className="container">
+              <div className="card">
+                <div className="list-group">
+                  {
+                    Object.keys(this.state.settings.actionDefinitions).map((keyString) => {
+                      const definition = this.state.settings.actionDefinitions[keyString];
+                      return(
+                        <div key={keyString} className="list-group-element">
+                          <div>
+                            {keyString}
+                          </div>
+                          <div>
+                            {definition.type}
+                          </div>
+                          <div>
+                            {definition.template}
+                          </div>
+                        </div>
+                      );
+                    })
+                  }
                 </div>
               </div>
-            );
-          })
-        }
+            </div>
+          </div>
+        </main>
       </div>
     );
   }
